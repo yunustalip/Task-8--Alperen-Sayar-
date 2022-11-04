2db1f90ff1d234502863d86dfdc4bfee43e2f977
Yunus Talip Bozlagan <bozlagan.talip@gmail.com>
2022-11-05 00:40:08 +0300

    update

--- 3 - lab.py
+++ 3 - lab.py
@@ -1,122 +1,57 @@
 import time
 import random
 
-class lab():
+class Laboratuvar():
     
-    def __init__(self, tv_durum="Kapalı",tv_ses=0,kanal_listesi=["TRT"],kanal="TRT", mod="uydu", sesdurum=0):
+    def __init__(self, sinifno=random.randint(1,9999), ogretmen="", kontenjan="", dersgunu=""):
 
-        self.tv_durum=tv_durum
-        self.tv_ses=tv_ses
-        self.kanal_listesi=kanal_listesi
-        self.kanal= kanal
-        self.mod = mod
-        self.sesdurum = sesdurum 
+        self.sinifno=sinifno
+        self.ogretmen=ogretmen
+        self.kontenjan = kontenjan
+        self.dersgunu = dersgunu
+        self.ogrencisayi = 0
+        self.ogrencilistesi = []
 
-    def tv_ac(self):
-
-        if (self.tv_durum  == "Kapalı"):
-            print("Tv açılıyor")
-            self.tv_durum="Açık"
+    def ogrenci_ekle(self,ogrenciisim):
+        if self.kontenjan<=self.ogrencisayi:
+            print("Kontenjan doludur")
         else:
-            print("Tv zaten acik")
+            self.ogrencisayi += 1
+            self.ogrencilistesi.append(ogrenciisim)
+            print("Öğrenci derse eklendi")
 
-    def tv_kapat(self):
-        
-        if (self.tv_durum  == "Kapalı"):
-            print("Tv zaten kapalı")
-        
-        else:
-            print("Tv zaten kapaniyor")
-            self.tv_durum="Kapalı"
+    def ogrencilistesinigoster(self):
+        print("Derste kayıtlı öğrenciler:", self.ogrencilistesi)
     
-
-    def ses_ayari(self):
-
-        while True:
-
-            cevap=input("Sesi artır :>\nSesi azalt: <\nÇıkış: q")
-
-            if (cevap=='>'):
-                if self.tv_ses>=100:
-                    print("Max Ses")
-                else:
-                    self.tv_ses+=1
-                print("Tv Sesi: {}".format(self.tv_ses))
-            
-            elif (cevap=="<"):
-                if (self.tv_ses<=0):
-                    print("Min ses")
-                else:
-                    self.tv_ses-=1
-                print("tv_ses: {}",format(self.tv_ses))
-            elif (cevap=="q"):
-                break
-            else:
-                print("hatalı tuşlama")
-
-    def kanal_ekle(self,kanal):
-        if kanal in self.kanal_listesi:
-            print("Kanal zaten var")
-        else:
-            
-            print("Kanal ekleniyor...")
-            time.sleep(1)
-            
-
-            self.kanal_listesi.append(kanal)
-
-            print("Kanal listesi: {}".format(self.kanal_listesi))
-
-    def kanal_sec(self):
-
-        rastgele=random.randint(0,len(self.kanal_listesi)-1)
-
-        self.kanal=self.kanal_listesi[rastgele]
-        print(self.kanal)
-
-    def kanal_sil(self,kanal):
-        if kanal in self.kanal_listesi:
-            self.kanal_listesi.remove(kanal)
-            print(kanal, " silindi")
-        else:
-            print("Kanal zaten kayıtlı değil")
+    def ogrenci_sil(self,ogrenciisim):
+            self.ogrencisayi -= 1
+            self.ogrencilistesi.remove(ogrenciisim)
+            print("Öğrenci silindi")
     
-    def mod_degistir(self):
-        if self.mod=="iptv":
-            self.mod = "uydu"
-        else:
-            self.mod = "iptv"
+    def ogretmen_ogren(self):
+            print(f"{self.sinifno} nolu sınıfın öğretmeni: {self.ogretmen}")
 
-    def mute(self):
-        if self.tv_ses > 0:
-            self.sesdurum = self.tv_ses
-            self.tv_ses =0
-            print("ses kapatıldı")
-        else:
-            self.tv_ses = self.sesdurum
-            print("ses açıldı")
+    def ogretmen_degistir(self,ogretmenisim):
+        self.ogretmen = ogretmenisim
+        print(f"{self.sinifno} nolu sınıfın yeni öğretmeni: {ogretmenisim}")
     
-    def __len__(self):
-        return len(self.kanal_listesi)
+    def dersgunu_degistir(self,gun):
+        self.dersgunu = gun
+        print(f"{self.ogretmen} öğretmenin yeni labaratuvar dersi günü: {gun}")
     
-    def __str__(self):
-        return "Tv_durum: {}\ntv_ses: {}\nkanal listesi: {}\nkanal:{}".format(self.tv_durum,self.tv_ses,self.kanal_listesi,self.kanal)
 
 print("""
-
-        1. TV aç
-        2. TV kapat
-        3. Ses Ayarları
-        4. Kanal Ekle
-        5. Açık Kanalı Öğren
-        6. Kanal Sayısı
-        7. TV Bilgileri
-        8. Mod değiştir (ip tv / uydu)
+        1. Öğrenci Ekle
+        2. Öğrenci Sil
+        3. Öğrenci Listesi
+        4. Sınıfın Öğretmenini Öğren
+        5. Sınıfın Öğretmenini Değiştir
+        6. Sınıfın Ders Gününü Değiştir
 
 Çıkmak için q'ya bas.
 
 """)
-kumanda=Kumanda()
+lab=Laboratuvar(sinifno=1001,ogretmen="Hasan",kontenjan=20,dersgunu="Cuma")
 
 while True:
 
@@ -126,32 +61,27 @@ while True:
         print("Sonlandırılıyor")
      
     elif islem =="1":
-        kumanda.tv_ac()
+        ogrenciisim = input("Öğrenci ismi giriniz")
+        lab.ogrenci_ekle(ogrenciisim)
     
     elif islem=="2":
-        kumanda.tv_kapat()
+        ogrenciisim = input("Öğrenci ismi giriniz")
+        lab.ogrenci_sil(ogrenciisim)
 
     elif islem=="3":
-        kumanda.ses_ayari()
+        lab.ogrencilistesinigoster()
     
     elif islem=="4":
-
-        kanal_isimleri=input("Kanal isimlerini lütfen , ile ayırarak giriniz.")
-
-        x=kanal_isimleri.split(",")
-
-        for i in x:
-            kumanda.kanal_ekle(i)
+        lab.ogretmen_ogren()
     
     elif islem=="5":
-        kumanda.kanal_sec()
+        yeniogretmen = input("Yeni öğretmen ismi giriniz")
+        lab.ogretmen_degistir(yeniogretmen)
 
     elif islem=="6":
-        print("Kanal sayısı: ",len(kumanda))
-    
-    elif islem=="7":
-        print(kumanda)
-    
+        dersgunu_degistir = input("Yeni ders günü giriniz")
+        lab.ogretmen_degistir(dersgunu_degistir)
+       
     else:
         print("hatalı Tuşlama")
 
